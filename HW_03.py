dd1d23963911f015774ba2e6dae67a07ddfea4f2
Danil <danila_monster@mail,ru>
2022-10-16 13:52:19 +0300

    HW_04

--- HW_03.py
+++ HW_03.py
@@ -17,3 +17,22 @@ def sort_list(*args):
 
 
 print(sort_list("Иван", "Мария", "Петр", "Илья", "Марина", "Алина", "Бибочка"))
+
+########################### 2 var
+from itertools import groupby
+
+def thesaurus(*args):
+    if "" not in args:
+        return {ch: list(names) for ch, names in groupby(sorted(args), key=lambda i: i[0]) if ch}
+    return "Error"
+
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья", "Марина", "Петр", "Алина", "Бибочка"))
+
+
+#########################   3 var
+
+def thesaurus(*args):
+    if "" not in args:
+        return {ch[0]: list(filter(lambda el: el.startswith(ch[0]), args)) for ch in sorted(args)}
+    return "Error"
